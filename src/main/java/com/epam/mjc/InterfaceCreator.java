efd96a3ed44915b81a084350d232e66704e99e27
ElenaGeorgievska <elena_science@yahoo.com>
2023-09-15 01:39:28 +0200

    stage1_module5_task2_lambdaExpressions completed

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -1,8 +1,26 @@
 package com.epam.mjc;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
 public class InterfaceCreator {
 
     public Operation<Integer> divideBy(Integer divider) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return arg -> arg.stream()
+                .map(e -> e / divider)
+                .collect(Collectors.toList());
+
+    }
+
+    public static void main(String[] args) {
+        //List<Integer> result = new InterfaceCreator().divideBy(3).apply(numbers);
+
+        int divider = 3;
+        List<Integer> integers = Arrays.asList(3, 6, 9);
+        InterfaceCreator ic = new InterfaceCreator();
+        ic.divideBy(divider).apply(integers);
+        System.out.println(ic.divideBy(divider).apply(integers));
     }
 }
